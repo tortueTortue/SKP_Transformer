a9bcd46d9e2e5f15455202d9c767225b6c34e4d8
tortue <dxlatortue@gmail.com>
2021-04-07 13:58:24 -0400

    impl

--- dataset/cifar_data_loaders.py
+++ dataset/cifar_data_loaders.py
@@ -6,10 +6,14 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import random_split
 from torchvision.utils import make_grid
 
+from typing import Any, Callable, Optional, Tuple
+
 dataset = CIFAR10(root='data/', download=True, transform=ToTensor())
 test_dataset = CIFAR10(root='data/', train=False, transform=ToTensor())
 
 labels = dataset.classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'deer', 'frog', 'horse', 'ship', 'truck' ]
 
 torch.manual_seed(43)
 train_dataset, val_dataset = random_split(dataset, [int(len(dataset) * 0.85),
@@ -21,14 +25,59 @@ train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=4
 val_loader = DataLoader(val_dataset, batch_size*2, num_workers=4, pin_memory=True)
 test_loader = DataLoader(test_dataset, batch_size*2, num_workers=4, pin_memory=True)
 
+class CIFAR10WithIndices(CIFAR10):
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target, index) where target is index of the target class.
+        """
+        image, target = super().__getitem__(index)
+        return image, target, index
+
+
+class Cifar10Dataset:
+    def __init__(self, batch_size=128):
+        dataset = CIFAR10WithIndices(root='data/', download=True, transform=ToTensor())
+        test_dataset = CIFAR10WithIndices(root='data/', train=False, transform=ToTensor())
+        
+        self.labels = dataset.classes
+        self.classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                        'dog', 'frog', 'horse', 'ship', 'truck' ]
+
+
+        torch.manual_seed(43)
+        train_dataset, val_dataset = random_split(dataset, [int(len(dataset) * 0.85),
+                                                            int(len(dataset) * 0.15)])
+        self.train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+        self.val_loader = DataLoader(val_dataset, batch_size*2, num_workers=4, pin_memory=True)
+        self.test_loader = DataLoader(test_dataset, batch_size*2, num_workers=4, pin_memory=True)
+
+        self.dataset = dataset
+
+    def get_dataloaders(self, batch=10):
+        return self.train_loader, self.val_loader, self.test_loader
+
+    def __getitem__(self, id):
+        return self.dataset.__getitem__(id), id
+
+
 def run():
     torch.multiprocessing.freeze_support()
-    for images, _ in train_loader:
+
+    cifar10_data = Cifar10Dataset(batch_size=batch_size)
+
+    train_loader, _, _ = cifar10_data.get_dataloaders()
+
+    for images, _, idx in train_loader:
         print('images.shape:', images.shape)
         plt.figure(figsize=(16,8))
         plt.axis('off')
         plt.imshow(make_grid(images, nrow=16).permute((1, 2, 0)))
         break
 
-# if __name__ == '__main__':
-#     run()
+if __name__ == '__main__':
+    run()
--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-from src.models.baseline import train_baseline
+"""
+
+"""
+
+import torchvision.models as models
+
+from models.skp_Transformer import SKP_Transformer
+
+from training.training_manager import train_model
+from dataset.cifar_data_loaders import Cifar10Dataset
+from training.utils.utils import save_model, load_model, load_checkpoint
+from training.metrics.metrics import print_accuracy_per_class, print_accuracy, count_model_parameters
+import copy
+import time
 
 if __name__ == '__main__':
-    # Baseline
-    train_baseline()
-    #TODO add progress bar, more epochs and save model after a certain amount of epochs
-    # CPC v1
+    # TODO Add as config
+    project_name = "SKP_Transformer"
+    model_dir = f"E:/Git/{project_name}/models/trained_models/"
+    model_name = ""
+    checkpoint_dir = f"E:/Git/{project_name}/training/checkpoints/checkpoint_{model_name}.pt"
+    batch_size = 15
+    epochs = 36
+    cifar10_data = Cifar10Dataset(batch_size=batch_size)
+    classes = cifar10_data.classes
 
-    # CPC v2
+    """
+    M A I N
+    """
+
+    # SKP TRANSFORMER 100 epochs
+    skp = SKP_Transformer(8, 50000, len(classes))
+    print(f"Parameters {count_model_parameters(skp, False)}")
+    start_time = time.time()
+    save_model(train_model(epochs, skp, "SKP", cifar10_data, batch_size, model_dir), "SKP", model_dir)
+    print(f"Training time for {epochs} epochs : {time.time() - start_time}")
+    print_accuracy_per_class(skp, classes, batch_size, cifar10_data.test_loader)
+    print_accuracy(skp, classes, batch_size, cifar10_data.test_loader)
+
+
